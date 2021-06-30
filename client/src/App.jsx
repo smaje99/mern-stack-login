5f3388a4bb7b15e0fce7fafcc5278eaf0833c1cc
Sergio Majé <smajefranco@gmail.com>
2021-06-30 02:07:29 -0500

    Toastify configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import { AppRouter } from './routers/AppRouter';
 import { AuthProvider } from './auth/AuthProvider';
 import { Layout } from './components/layouts/Layout';
 
 export const App = () => (
-    <Router>
-        <AuthProvider>
-            <Layout>
-                <AppRouter />
-            </Layout>
-        </AuthProvider>
-    </Router>
+    <Fragment>
+        <Router>
+            <AuthProvider>
+                <Layout>
+                    <AppRouter />
+                </Layout>
+            </AuthProvider>
+        </Router>
+        <ToastContainer />
+    </Fragment>
 );
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { App } from './App';
 
